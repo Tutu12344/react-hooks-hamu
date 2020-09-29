0bdfc9301fb4112f4b5cb47ead9126e3a27a87c3
Tutu12344 <padde.cross@gmail.com>
2020-09-30 08:15:23 +0900

    usestate merge

--- src/App.js
+++ src/App.js
@@ -4,17 +4,31 @@ import "./App.css";
 
 const App = (props) => {
   // const initialStates = {};
-  const [name, setName] = useState(props.name);
-  const [price, setPrice] = useState(props.price);
+  // const [name, setName] = useState(props.name);
+  const [state, setState] = useState(props);
+  const { name, price } = state;
+  // const [price, setPrice] = useState(props.price);
   // console.log(output);
+  const reset = () => {
+    setState(props);
+  };
   return (
     <div>
       <p>
         現在の{name}は{price}です
       </p>
-      <button onClick={() => setPrice(price + 1)}>+1</button>
-      <button onClick={() => setPrice(price - 1)}>-1</button>
-      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <button onClick={() => setState({ ...state, price: state.price + 1 })}>
+        +1
+      </button>
+      <button onClick={() => setState({ ...state, price: state.price - 1 })}>
+        -1
+      </button>
+      <button onClick={reset}>reset</button>
+
+      <input
+        value={name}
+        onChange={(e) => setState({ ...state, name: e.target.value })}
+      />
     </div>
   );
 };
